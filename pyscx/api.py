b561c586f2a369dca834cb37966ae34a9f89a4bd
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-02-08 02:16:10 +1000

    API methods proto

--- pyscx/api.py
+++ pyscx/api.py
@@ -11,11 +11,11 @@ class Server(Enum):
 
 
 class APISession(requests.Session):
-    def __init__(self, server_url):
+    def __init__(self, server_url: str):
         super().__init__()
         self.base_url = server_url.rstrip("/")
 
-    def include_token(self, token) -> None:
+    def include_token(self, token: str) -> None:
         self.headers.update({"Authorization": f"Bearer {token}"})
 
     def request(self, method, url, *args, **kwargs):
@@ -77,5 +77,5 @@ class ApplicationAPI(API):
 
     emission: any = ...
     character: any = ...
-    auction: any = ...
     clans: any = ...
+    auction: any = ...
---
+++ pyscx/methods.py
@@ -0,0 +1,35 @@
+class APIMethods(object):
+    def __init__(self, session):
+        self.session = session
+
+
+class RegionsAPI(APIMethods):
+    pass
+
+
+class EmissionsAPI(APIMethods):
+    pass
+
+
+class AuctionAPI(APIMethods):
+    pass
+
+
+class FriendsAPI(APIMethods):
+    pass
+
+
+class CharacterMethods(APIMethods):
+    pass
+
+
+class CharactersMethods(APIMethods):
+    pass
+
+
+class ClanMethods(APIMethods):
+    pass
+
+
+class ClansMethods(APIMethods):
+    pass
