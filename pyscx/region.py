4a1cbf3917921fefc06942f9ae1ad662cfa948d6
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-02-07 18:21:17 +1000

    Init mechanism changed

--- pyscx/region.py
+++ pyscx/region.py
@@ -1,13 +1,14 @@
 class Region(object):
     def __init__(self, id: int = None, name: str = None, *args, **kwargs) -> None:
         # The ability to transfer documents explicitly or as an unpacked dictionary
-        self.id = id if id is not None else kwargs.get("id")
-        self.name = name if name is not None else kwargs.get("name")
+        for attr, value in {"id": id, "name": name}.items():
+            resolved_value = value or kwargs.get(attr)
+            setattr(self, attr, resolved_value)
 
-        if self.id is None or self.name is None:
+        if None in (self.id, self.name):
             raise ValueError(
                 "Both 'id' and 'name' must be provided either explicitly or in kwargs."
             )
 
     def __repr__(self) -> str:
-        return f"Region<name={self.name}>"
+        return f"Region<id={self.id}>"
