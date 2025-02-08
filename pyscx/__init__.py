34a27bee74c77cf380c22bddd65b095c57ec0ee4
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-02-09 02:07:28 +1000

    Limiting the import of internal modules

--- pyscx/__init__.py
+++ pyscx/__init__.py
@@ -1,4 +1,11 @@
+import sys
 from .api import Server, API
 
 
 __all__ = ("Server", "API")
+
+__excluded__ = ("http", "api", "methods")
+
+for module in __excluded__:
+    if module in sys.modules:
+        raise ImportError(f"Module {module} is for internal use only.")
