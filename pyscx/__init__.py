f86c13dd45fde49459434defb9225216cf1f4fb3
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-02-10 01:36:40 +1000

    Deleted useless

--- pyscx/__init__.py
+++ pyscx/__init__.py
@@ -1,18 +1,4 @@
-import builtins
 from .api import Server, API
 
 
 __all__ = ("Server", "API")
-__excluded_modules__ = ("http", "api", "methods")
-
-
-original_import = builtins.__import__
-
-
-def custom_import(name, *args, **kwargs):
-    if name in __excluded_modules__:
-        raise ImportError(f"Module {name} is for internal use only.")
-    return original_import(name, *args, **kwargs)
-
-
-builtins.__import__ = custom_import
