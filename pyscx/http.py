b0b70f958bf1b092a4f6209157a4012d85881986
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-02-10 22:26:37 +1000

    Http module documentation (#12)

--- pyscx/http.py
+++ pyscx/http.py
@@ -2,6 +2,12 @@ import requests
 
 
 class APISession(requests.Session):
+    """Custom wrapper around the Session class from the `requests` module.
+
+    Allows storing the base URL of the server and sending requests by
+    specifying only the resource path.
+    """
+
     def __init__(self, server_url: str):
         super().__init__()
         self.base_url = server_url.rstrip("/")
