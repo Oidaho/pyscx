01259d451a50e36a73b2f06aad61f99c3ae25b88
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-02-10 20:16:11 +1000

    Token module docstrings (#12)

--- pyscx/token.py
+++ pyscx/token.py
@@ -2,11 +2,21 @@ from enum import Enum
 
 
 class TokenType(Enum):
+    """Access token type enum."""
+
     USER = "user"
     APPLICATION = "application"
 
 
 class Token(object):
+    """The general class of the API access token.
+    The interface for transferring a token to an API object.
+
+    Initial Args:
+        value (str): Access token.
+        type (TokenType): Access token type.
+    """
+
     def __init__(self, value: str, type: TokenType) -> None:
         self.value = value
         self.type = type
